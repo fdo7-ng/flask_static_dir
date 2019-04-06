85adcd3b332cd8de83707be155f346d3c1a23246
fdo-ng <blizzytemp@hotmail.com>
2019-04-06 07:15:44 -0400

    created route to add and delete files on alternate static directory

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
+#Sample script server files from outside of the project folder
+
 import os
 
 from flask import Flask
@@ -21,5 +23,34 @@ def serve_file_in_dir(path):
         path = os.path.join(path, 'index.html')
 
     return send_from_directory(static_file_dir, path)
+
+# Create files in the defined static dictory
+@app.route('/create/<file>')
+def create(file):
+    print("check if file exist in")
+    if os.path.isfile(os.path.join(static_file_dir, file)):
+        print("Found file : " , file)
+        return send_from_directory(static_file_dir, file)
+    else:
+        print("Not Found creatting file: ", file)
+        filepath = os.path.join(static_file_dir, file)
+        f = open(filepath,"w+")
+        f.write("Creating this file via api")
+        f.close()
+        return send_from_directory(static_file_dir, file)
+
+
+# Delete files in defined static directory
+@app.route('/delete/<file>')
+def delete(file):
+    print("check if file exist in")
+    if os.path.isfile(os.path.join(static_file_dir, file)):
+        print("Found file : " , file , " deleting")
+        os.remove( os.path.join(static_file_dir, file))
+        return "File Removed "
+    else:
+        print("Not Found creatting file: ", file)
+        return "File Not found"
+
 
 app.run(host='0.0.0.0',port=8080,debug=True)
